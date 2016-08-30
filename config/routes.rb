01cf40b00106ac126a21cab4602c83ed81579fb5
Abdul Muhaimin <abdulmuhaimin.work@gmail.com>
2016-08-30 15:42:15 +0800

    create controller: Products, Carts, and OrderItems

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,21 @@
 Rails.application.routes.draw do
-  root to: 'landing#index'
+  # get 'order_items/create'
+  #
+  # get 'order_items/update'
+  #
+  # get 'order_items/destroy'
+  #
+  # get 'carts/show'
+  #
+  # get 'products/index'
+  #
+  # root to: 'landing#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root to: 'landing#index'
-  get :about, to: 'static_pages#about'
+  # root to: 'landing#index'
+  # get :about, to: 'static_pages#about'
+  resources :products, only: [:index]
+  resource :cart, only: [:show]
+  resources :order_items, only: [:create, :update, :destroy]
+  root to: 'products#index'
 
 end
